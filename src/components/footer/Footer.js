80a812c8eacbd03af284f680274aa72a55ff8bca
David Vittor <david.vittor@slyp.com.au>
2020-04-29 01:06:31 +1000

    Small improvements

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,10 +7,10 @@ const Footer = (props) => {
       <div className="footer">
         <div className="footer-links">
           <Link to="/about">About</Link><span className="footer-link-slash">/</span>
-          <a href="/bulletin/bulletin.pdf" target="_blank">Bulletin</a><span className="footer-link-slash">/</span>
+          <Link to="/events">What's On</Link><span className="footer-link-slash">/</span>
           <Link to="/sermons">Sermons</Link><span className="footer-link-slash">/</span>
           <Link to="/contact">Contact</Link><span className="footer-link-slash">/</span>
-          {/* <Link to="/live">Watch Live</Link> */}
+          <Link to="/giving">Giving</Link>
         </div>
         <div className="row">
           <div className="col-sm">

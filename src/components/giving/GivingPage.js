3be86ab3b3e8755c1cd543c5ec279d62f7f4c88f
David Vittor <david.vittor@slyp.com.au>
2020-07-16 22:08:27 +1000

    Updated giving page

--- src/components/giving/GivingPage.js
+++ src/components/giving/GivingPage.js
@@ -22,8 +22,7 @@ class GivingPage extends React.Component {
             <BibleVerse reference="2CO.9.7" text="2 Corinthians 9:7" />
           </p>
           <p>A number of people have been asking about how they can continue to support the Lord’s work financially while the normal weekly meetings are suspended.</p>
-          <p>Please contact our church treasurer, Justin Portelli, at <a href="mailto:treasurer@fellowship.org.au?subject=Enquiry%20about%20giving&body=Hi%20Gary,%0A%0AI%20would%20like%20to%20enquire%20about%20the%20process%20for%20supporting%20the%20Lord’s%20work%20financially while%20the%20normal%20weekly%20meetings%20are%20suspended.%0A%0AThank%20You">treasurer@fellowship.org.au</a> and details of how you can do this will be sent to you by email.</p>
-          <p>If you are a tax payer and do not already give by Gift Aid, we would encourage you to do so. You can fill in the form online <a href="https://forms.gle/yaRkusVi7LDHJkdQ7" target="_blank" rel="noopener noreferrer">here</a>. Alternatively you can download the gift form at the bottom of this page. Please print, complete, scan and return to the treasurer via email.</p>
+          <p>Please contact our church treasurer, Justin Portelli, at <a href="mailto:treasurer@fellowship.org.au?subject=Enquiry%20about%20giving&body=Dear%Pastor,%0A%0AI%20would%20like%20to%20enquire%20about%20the%20process%20for%20supporting%20the%20Lord’s%20work%20financially.%0A%0AThank%20You">treasurer@fellowship.org.au</a> and details of how you can do this will be sent to you by email.</p>
           <br />
           <a href="media/donation-details.pdf">Donation Details (PDF)</a>
         </div>

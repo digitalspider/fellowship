9604cf0d4978bd8fd6b3f658f078303e6ba3a72d
David Vittor <david.vittor@slyp.com.au>
2020-08-11 22:35:06 +1000

    Dynamic newsletters

--- src/components/resources/ResourcesPage.js
+++ src/components/resources/ResourcesPage.js
@@ -1,40 +1,58 @@
 import React from 'react';
 import moment from 'moment';
 
+const DATE_FORMAT_ISO = 'YYYYMMDD';
 const DATE_FORMAT_AUD = 'DD MMM YYYY';
-// const DATE_FORMAT_ISO = 'YYYYMMDD';
-const ResourcesSection = (props) => {
-  const publishDay = moment().day('Thursday');
-  return (
-    <div className="page">
-      <h2>Newsletters</h2>
-      <ul>
-        <li>
-          <h4><a target="_blank" href='/newsletters/20200723.pdf'>{publishDay.format(DATE_FORMAT_AUD)} (Current)</a></h4>
-        </li>
-        <li>
-          <h4><a target="_blank" href='/newsletters/20200716.pdf'>{publishDay.clone().add(-7, 'day').format(DATE_FORMAT_AUD)}</a></h4>
-        </li>
-        <li>
-          <h4><a target="_blank" href='/newsletters/20200709.pdf'>{publishDay.clone().add(-14, 'day').format(DATE_FORMAT_AUD)}</a></h4>
-        </li>
-        <li>
-          <h4><a target="_blank" href='/newsletters/20200702.pdf'>{publishDay.clone().add(-21, 'day').format(DATE_FORMAT_AUD)}</a></h4>
-        </li>
-      </ul>
+const GITHUB_NEWSLETTERS_URL = 'https://api.github.com/repos/digitalspider/fellowship/contents/public/newsletters';
 
-      <h2>Links</h2>
-      <table className="table">
-        <thead>
-          <tr><th>Link</th><th>Description</th></tr>
-        </thead>
-        <tbody>
-          <tr><td><a rel="noopener noreferrer" target="_blank" href="https://peacewithgod.net/">Peace with God</a></td><td>Find peace with God</td></tr>
-        </tbody>
-      </table>
-      <br />
-    </div>
-  )
+// String -> [String]
+async function getNewsletters() {
+  const response = await fetch(GITHUB_NEWSLETTERS_URL);
+  const data = await response.json();
+  console.log(data);
+  return data.map((file) => file.name).filter((filename) => filename.slice(-4) === '.pdf').reverse();
+}
+
+
+class ResourcesSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newsletters: [],
+    };
+  }
+
+  async componentDidMount(_prevProps) {
+    const newsletters = await getNewsletters();
+    this.setState({ newsletters });
+
+  }
+
+  render() {
+    return (
+      <div className="page">
+        <h2>Newsletters</h2>
+        <ul>
+          {this.state.newsletters.map((value, index) => {
+            return <li key={index}>
+              <h4><a target="_blank" rel="noreferrer" href={'/newsletters/' + value}>{moment(value, DATE_FORMAT_ISO).format(DATE_FORMAT_AUD) + (index === 0 ? ' (Current)' : '')}</a></h4>
+            </li>
+          })}
+        </ul>
+
+        <h2>Links</h2>
+        <table className="table">
+          <thead>
+            <tr><th>Link</th><th>Description</th></tr>
+          </thead>
+          <tbody>
+            <tr><td><a rel="noopener noreferrer" target="_blank" href="https://peacewithgod.net/">Peace with God</a></td><td>Find peace with God</td></tr>
+          </tbody>
+        </table>
+        <br />
+      </div>
+    )
+  }
 }
 
 export default ResourcesSection;

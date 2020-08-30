2ebd477dc46c979308b57c78a129d88ff3c21568
David Vittor <david.vittor@slyp.com.au>
2020-08-31 08:34:35 +1000

    Updated Events Page

--- src/components/events/EventsPage.js
+++ src/components/events/EventsPage.js
@@ -11,16 +11,28 @@ const WhatsOnSection = (props) => {
         <h2 className="title">Sunday Meetings</h2>
         <p className="highlightText">Morning service at 10.00am</p>
 
-        <h2 className="title">Midweek Meetings</h2>
+        <h2 className="title">Regular Ministries</h2>
         <table className="table">
           <tbody>
-            <tr><td>Women's Meeting</td><td>Last Tuesday of each month at 8pm</td></tr>
-            <tr><td>Bible study and care groups</td><td>Friday fortnightly 7:30pm</td></tr>
-            <tr><td>IGNITE Young adults</td><td>Friday fortnightly 7:30pm</td></tr>
-            <tr><td>EPIC Youth</td><td>First Thursday of each month at 7:00pm</td></tr>
+            <tr><th>Event</th><th>What</th><th>When</th></tr>
+            <tr><td>Care Groups</td><td>Bible studies for families</td><td>Friday Fortnightly, 7:30PM</td></tr>
+            <tr><td>IGNITE</td><td>Young adults bible study and social events</td><td>Friday Fortnightly, 7:30PM</td></tr>
+            <tr><td>EPIC Youth</td><td>Teens bible lessons and social events</td><td>Last Friday of the Month, 7PM</td></tr>
+            <tr><td>Men’s Coffee Club</td><td>Coffee meet up for the men of the church</td><td>Last Saturday of the Month, 8:30AM</td></tr>
+          </tbody>
+        </table>
+        <h2 className="title">Annual Ministries</h2>
+        <table className="table">
+          <tbody>
+            <tr><th>Event</th><th>What</th><th>When</th></tr>
+            <tr><td>Christmas</td><td>A celebration of the birth of Jesus Christ</td><td>Late December</td></tr>
+            <tr><td>Easter Friday and Sunday</td><td>A rememberence of the death and resurrection of Jesus Christ</td><td>Usually in April</td></tr>
+            <tr><td>Holiday Bible Club</td><td>A week long school-holiday event for children aged 12 and younger</td><td>September/October school holidays</td></tr>
+            <tr><td>AFBC Conferences</td><td>A 3-day annual conference</td><td>June/July</td></tr>
           </tbody>
         </table>
         <br />
+
       </div>
     </Page>
   )

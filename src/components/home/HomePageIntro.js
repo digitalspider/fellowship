33ed9aad44dfc502cb08a57ef400a759d0dc716f
David Vittor <david.vittor@slyp.com.au>
2020-09-12 09:42:15 +1000

    Updated purpose_statement

--- src/components/home/HomePageIntro.js
+++ src/components/home/HomePageIntro.js
@@ -8,11 +8,15 @@ const HomePageIntro = (props) => {
     <div className="row intro">
       <div className="column home-page-intro">
         <h2 className="title who-we-are">Who We Are</h2>
-        <p>Fellowship Baptist Church is a medium-sized family church, serving the suburbs of Blacktown. Whether you are curious about Christianity or looking for a church home, you will find us to be friendly and helpful—a real fellowship. You are welcome to join us this Sunday! We show love and reverence for God in our songs, prayers, giving, and messages from His Word.</p>
+        <p className="welcome-text">
+          Fellowship Baptist Church is a medium-sized family church, serving the suburbs of Blacktown. 
+          Whether you are curious about Christianity or looking for a church home, you will find us to be friendly and helpful—a real fellowship.
+          You are welcome to join us this Sunday! We show love and reverence for God in our songs, prayers, giving, and messages from His Word.
+        </p>
         <Link to="/about" className="btn-about-us">Find out more</Link>
       </div>
       <div className="column">
-        <img className="mission-statement-image" src="/img/mission_statement.jpg" alt="FBC Mission Statement" />
+        <img className="purpose-statement-image" src="/img/purpose_statement.jpg" alt="FBC Purpose Statement" />
       </div>
     </div>
   );

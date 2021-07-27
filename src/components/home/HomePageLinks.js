e2393188c49c17a99597da074a97e1c19770348b
dvittor-bigw <dvittor2@bigw.com>
2021-07-27 22:37:41 +1000

    Fixed church ministries link

--- src/components/home/HomePageLinks.js
+++ src/components/home/HomePageLinks.js
@@ -9,7 +9,7 @@ function HomePageLinks(props) {
       <Link to="/covid19" className="tile homepage-text covid">
         <div className="tile-text">COVID-19 Update</div>
       </Link>
-      <Link to="/about/ministry" className="tile homepage-text church">
+      <Link to="/about/church" className="tile homepage-text church">
         <div className="tile-text">Church Ministries</div>
       </Link>
       <Link to="/sermons" className="tile homepage-text sermon">

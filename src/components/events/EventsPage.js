6f7971fa19c33592dd9b3358fe49839d0d0f8452
Development Team <8529150+digitalspider@users.noreply.github.com>
2022-07-20 11:52:14 +1000

    Updated calendar

--- src/components/events/EventsPage.js
+++ src/components/events/EventsPage.js
@@ -15,7 +15,7 @@ function WhatsOnSection(props) {
           <a class="btn btn-lg" href="/about/church">Church Ministries</a>
         </p>
         <div className="calendar">
-          <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23e2e2e2&amp;ctz=Australia%2FSydney&amp;src=b25oczllajB2MWh1aGtwcmdlamhqY2U0bTBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23e2e2e2&amp;showTitle=1&amp;title=FBC%20Events&amp;showDate=1&amp;showTabs=1&amp;showCalendars=0&amp;showTz=0&amp;showPrint=0" width="100%" height="600" frameborder="0" scrolling="no" title="calendar"></iframe>
+          <iframe src="https://calendar.google.com/calendar/embed?src=l4bqslhrtjsf522dvmcirodf5c%40group.calendar.google.com&amp;ctz=Australia%2FSydney&amp;height=600&amp;wkst=1&amp;bgcolor=%23e2e2e2&amp;color=%23e2e2e2&amp;showTitle=1&amp;title=FBC%20Events&amp;showDate=1&amp;showTabs=1&amp;showCalendars=0&amp;showTz=0&amp;showPrint=0" width="100%" height="600" frameborder="0" scrolling="no" title="calendar"></iframe>
         </div>
 
       </div>

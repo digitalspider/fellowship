f2af8b639d4866b24172d8bfb40b3670afa6be73
David Vittor <david.v@karta.com.au>
2022-09-11 22:00:18 +1000

    Updated enzyme dependencies

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,5 @@
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16.2';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 import 'jest-enzyme';
 
 configure({ adapter: new Adapter() });

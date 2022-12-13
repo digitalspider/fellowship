4245b127b79c600c795aabaf0c824093e7f7e15c
David Vittor <david.v@karta.com.au>
2022-12-13 22:18:47 +1100

    Changed fonts on pages from Lato to Calibri

--- src/components/events/EventsPage.js
+++ src/components/events/EventsPage.js
@@ -30,6 +30,7 @@ function WhatsOnSection(props) {
         <div className="calendar">
           <iframe
             src="https://calendar.google.com/calendar/embed?src=l4bqslhrtjsf522dvmcirodf5c%40group.calendar.google.com&amp;ctz=Australia%2FSydney&amp;height=600&amp;wkst=1&amp;bgcolor=%23e2e2e2&amp;color=%23e2e2e2&amp;showTitle=1&amp;title=FBC%20Events&amp;showDate=1&amp;showTabs=1&amp;showCalendars=0&amp;showTz=0&amp;showPrint=0"
+            // src="https://calendar.planningcenteronline.com/icals/eJxj4ajmsGLLz2Te0sJoxZVanF9QUs1pxV6R5qnEZWRuamzGZsXmGmLFVuypxJ-YkxOfWpaaV1LMZs0BFCvNZGa0ELFiLyvxVOIDSZZk5qZC5LgLEosSc4urGQC2aRn1a85bafe4dc73bbb7414db27b34206d273880e252"
             width="100%"
             height="600"
             frameBorder="0"

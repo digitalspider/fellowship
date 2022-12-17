25b9e26c9e0daa759cb3c3b2b72e56ebbcc30a59
David Vittor <david.v@karta.com.au>
2022-12-17 21:58:02 +1100

    Update church ministries

--- src/components/about/WhatToExpect.js
+++ src/components/about/WhatToExpect.js
@@ -3,6 +3,11 @@ import React from 'react';
 function WhatToExpect(props) {
   return (
     <div>
+      <h2>Our Worship Services</h2>
+      <p>You will find that our worship services are designed to be friendly, edifying, Scripture-oriented, and Christ-honouring. We strive to do things well but not to be overly formal. Our worship-music style is a fairly conservative blend, making use of Psalms (Scripture songs), hymns (songs of praise to God & of doctrinal truth), and spiritual songs (songs of testimony/reflection of the Christian experience & expressions of personal faith, love, and hope) both old and new. We use a range of instruments from one service to another, including piano, electric keyboard, acoustic & bass guitars, and various orchestral instruments. Our blend varies from week to week, so it will take a few visits to get an accurate idea of what constitutes our "style."</p>
+      <p>We normally use either the NKJV or ESV translations of the Bible for preaching and Scripture readings. You are welcome to bring any translation that suits you.</p>
+      <p>There is no "dress code," but we do try to be modest and respectful. You will see a wide range of attire, from suits or dresses to jeans and casual tops. Please dress according to your own comfort and/or convictions.</p>
+
       <h2>Parking</h2>
       <p>There are parking spaces both in the front and back of the church building. You are also welcome to park on the grass on the side of the building or on the street. Our greeters are happy to assist with the drop-off of anyone with physical limitations. Please do not hesitate to get their attention and ask for help.</p>
 
@@ -14,11 +19,6 @@ function WhatToExpect(props) {
       Children aged 4 through 10 stay with their families in the worship service until time for the message (Sunday morning).
       At that time, it will be announced that children are dismissed to Children's Church.
       Your children can easily find their way by joining the stampede, but you are very welcome to accompany them to see where you will collect them after the service.</p>
-
-      <h2>Our Worship Services</h2>
-      <p>You will find that our worship services are designed to be friendly, edifying, Scripture-oriented, and Christ-honouring. We strive to do things well but not to be overly formal. Our worship-music style is a fairly conservative blend, making use of Psalms (Scripture songs), hymns (songs of praise to God & of doctrinal truth), and spiritual songs (songs of testimony/reflection of the Christian experience & expressions of personal faith, love, and hope) both old and new. We use a range of instruments from one service to another, including piano, electric keyboard, acoustic & bass guitars, and various orchestral instruments. Our blend varies from week to week, so it will take a few visits to get an accurate idea of what constitutes our "style."</p>
-      <p>We normally use either the NKJV or ESV translations of the Bible for preaching and Scripture readings. You are welcome to bring any translation that suits you.</p>
-      <p>There is no "dress code," but we do try to be modest and respectful. You will see a wide range of attire, from suits or dresses to jeans and casual tops. Please dress according to your own comfort and/or convictions.</p>
     </div>
   )
 }
